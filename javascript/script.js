07ccb50c3cb470c59ba21625bf14094b78307c12
Thisbeepinyourteeth <159531769+Thisbeepinyourteeth@users.noreply.github.com>
2024-06-23 23:24:23 +0100

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -7,35 +7,3 @@ document.addEventListener('DOMContentLoaded', () => {
         mainMenu.classList.toggle('active');
     });
 
-    // Smooth scrolling for navigation links
-    document.querySelectorAll('.navbar a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href');
-            document.querySelector(targetId).scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
-    });
-
-    // Highlight the current section in the navigation bar
-    const sections = document.querySelectorAll('section');
-    const navLinks = document.querySelectorAll('.navbar ul li a');
-
-    window.addEventListener('scroll', () => {
-        let currentSection = '';
-        sections.forEach(section => {
-            const sectionTop = section.offsetTop;
-            if (pageYOffset >= sectionTop - 60) {
-                currentSection = section.getAttribute('id');
-            }
-        });
-
-        navLinks.forEach(link => {
-            link.classList.remove('active');
-            if (`#${currentSection}` === link.getAttribute('href')) {
-                link.classList.add('active');
-            }
-        });
-    });
-});
